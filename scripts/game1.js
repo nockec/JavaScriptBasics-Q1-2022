64426561cbb4d76321d9876dc89a95c7a3444a60
nockec <nockec@s.dcsdk12.org>
2022-08-22 20:25:13 -0600

    added variables and stated guessing game

---
+++ scripts/game1.js
@@ -0,0 +1,24 @@
+//@ts-cheak
+
+
+const button = document.getElementById("game");
+
+button?.addEventListener("click", function () {
+ //console.log("clicked");
+let answer = 7;
+
+
+
+ let guess = 
+ (prompt("pick a number from 1-10"));
+
+if(answer == guess) (
+    alert("you win !")
+    );
+
+
+ console.log(guess)
+
+
+});
+
