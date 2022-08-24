71a53de5c84f220167fe16a9ec6cf50194c299fc
nockec <nockec@s.dcsdk12.org>
2022-08-24 15:57:54 -0600

    3 lives

--- scripts/game1.js
+++ scripts/game1.js
@@ -1,24 +1,35 @@
-//@ts-cheak
+//@ts-check
 
+const button = document.getElementById("guessing-game");
 
-const button = document.getElementById("game");
+console.log(button);
 
 button?.addEventListener("click", function () {
- //console.log("clicked");
-let answer = 7;
+	// console.log("Clicked!");
+	// random returns a number between 0 and 1
+	// multiply it by 10 and add then get the whole
+	// number using floor.
+	let answer = Math.floor(Math.random() * 10 + 1);
+	let numberOfGuesses = 3;
+	let usedGuesses = 1;
+	let isGameOver = false;
 
+	while (isGameOver == false) {
+		let guess = Number(prompt("Pick a number between 1 and 10"));
+		usedGuesses = usedGuesses + 1;
 
-
- let guess = 
- (prompt("pick a number from 1-10"));
-
-if(answer == guess) (
-    alert("you win !")
-    );
-
-
- console.log(guess)
-
-
+		if (answer == guess) {
+			alert("You won!");
+			isGameOver = true;
+		} else if (usedGuesses > numberOfGuesses) {
+			alert("You lose! No more guesses!");
+			isGameOver = true;
+		} else {
+			if (guess < answer) {
+				alert("Too low! Guess again.");
+			} else {
+				alert("Too high! Guess again.");
+			}
+		}
+	}
 });
-
