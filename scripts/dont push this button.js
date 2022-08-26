c09bf7d5541e2010cde08e1e87d907c0ee0dcce2
nockec <nockec@s.dcsdk12.org>
2022-08-26 15:43:47 -0600

    dont push this button

---
+++ scripts/dont push this button.js
@@ -0,0 +1,28 @@
+//@ts-check
+const push = document.getElementById("dont push this button");
+
+push?.addEventListener("click", function () {
+     alert("dont push this button !!!!!!!")
+     alert("dont push this button !!!!!!!")
+     alert("dont push this button !!!!!!!")
+     alert("dont push this button !!!!!!!")
+     alert("dont push this button !!!!!!!")
+     alert("dont push this button !!!!!!!")
+     alert("dont push this button !!!!!!!")
+     alert("dont push this button !!!!!!!")
+     alert("dont push this button !!!!!!!")
+    alert("i told you dont push")
+    prompt("why did you push the button")
+        
+    console.log(push);
+    
+});
+
+
+
+
+
+
+
+
+
