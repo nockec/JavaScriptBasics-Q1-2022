6c4de8ca2120fa1384f00e5357fe1885639180f0
nockec <nockec@s.dcsdk12.org>
2022-08-31 15:45:31 -0600

    fineshed

--- scripts/app.js
+++ scripts/app.js
@@ -14,10 +14,10 @@ class Block{
         this.x = x;
 		this.y = y;
 		this.color = "red";
-		this.width = 10;
-		this.height = 10;
-		this.xdir = 5;
-		this.ydir = 5;
+		this.width = 50;
+		this.height = 50;
+		this.xdir = 2;
+		this.ydir = 2;
 	}
 	draw(){
 		if( this.x < 0 || this.x + this.width > canvas.width ){
@@ -34,47 +34,39 @@ class Block{
 	}
 }
 
-let Block1 = new Block(0,0);
-let Block2 = new Block(10,10);
-let Block3 = new Block(20,20);
-let Block4 = new Block(30,30);
-let Block5 = new Block(40,40);
-let Block6 = new Block(50,50);
-let Block7 = new Block(60,60);
-let Block8 = new Block(70,70);
-let Block9 = new Block(80,80);
-let Block10 = new Block(90,90);
-let Block11 = new Block(100,100);
-let Block12 = new Block(110,110);
-let Block13 = new Block(120,120);
-let Block14 = new Block(130,130);
-let Block15 = new Block(140,140);
-let Block16 = new Block(150,150);
-let Block17 = new Block(160,160);
-let Block18 = new Block(170,170);
-let Block19 = new Block(180,180);
+const numberofblocks = 10;
+let blocks = [];
+let color = [
+	"red",
+	"blue",
+	"yellow",
+	"green",
+	"indigo",
+	"violet",
+	"orange",
+];
+
+
+for (let index = 0; index < numberofblocks; index++) {
+	let w = Math.random()*30+10;
+	let h = Math.random()*30+10;
+	let x = Math.random()*(canvas.width-w);
+	let y = Math.random()*(canvas.height-h);
+	let colorIndex = Math.floor(Math.random()* color.length)
+	let block = new Block(x,y);
+	block.width= w;
+	block.height= h;
+	block.color = color[colorIndex];
+	blocks.push(block);
+
+}
+
 const animate = function () {
 	ctx?.clearRect(0, 0, canvas.width, canvas.height);
-Block1.draw();
-Block2.draw();
-Block3.draw();
-Block4.draw();
-Block5.draw();
-Block6.draw();
-Block7.draw();
-Block8.draw();
-Block9.draw();
-Block10.draw();
-Block11.draw();
-Block12.draw();
-Block13.draw();
-Block14.draw();
-Block15.draw();
-Block16.draw();
-Block17.draw();
-Block18.draw();
-Block19.draw();
 
+	blocks.forEach((b) =>{b.draw();
+	
+	})
 
 	requestAnimationFrame(animate);
 };
--- scripts/dont push this button.js
+++ scripts/dont push this button.js
@@ -12,7 +12,8 @@ push?.addEventListener("click", function () {
      alert("dont push this button !!!!!!!")
      alert("dont push this button !!!!!!!")
     alert("i told you dont push")
-    prompt("why did you push the button")
+    
+    
     
 });
 
